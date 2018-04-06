e2340a78eb6bbcb95fed6230a0032552889f735e
kimromi <kimromi4@gmail.com>
2018-04-06 19:02:46 +0900

    Implement Response class

---
+++ src/Client/Response.php
@@ -0,0 +1,19 @@
+<?php
+namespace Muumuu\Client;
+
+class Response
+{
+    private $response;
+
+    public function __construct(\GuzzleHttp\Psr7\Response $response) {
+        $this->response = $response;
+    }
+
+    public function statusCode() {
+        return $this->response->getStatusCode();
+    }
+
+    public function body() {
+        return json_decode($this->response->getBody(), true); // array
+    }
+}
