b26916c4b4b6a802ec3fda122e70603bfd2a4ccd
kimromi <kimromi4@gmail.com>
2018-04-06 20:46:27 +0900

    Add token to Config

--- src/Client/Config.php
+++ src/Client/Config.php
@@ -4,21 +4,32 @@ namespace Muumuu\Client;
 class Config
 {
     private static $defaultEndpoint = null;
+    private static $defaultToken = null;
     private $endpoint;
+    private $token;
 
     public function __construct(array $config)
     {
         $this->endpoint = isset($config['endpoint']) ? $config['endpoint']
                                                      : self::$defaultEndpoint ?: 'https://muumuu-domain.com/api/v1';
+        $this->token = isset($config['token']) ? $config['token'] : null;
     }
 
     public static function set(array $config)
     {
         self::$defaultEndpoint = $config['endpoint'];
+        if (isset($config['token'])) {
+            self::$defaultToken = $config['token'];
+        }
     }
 
     public function endpoint()
     {
         return $this->endpoint;
+    }
+
+    public function token()
+    {
+        return $this->token;
     }
 }
